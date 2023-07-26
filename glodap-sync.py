c369ae45eecaf354ce3847dc8a054174ab9b4fc3
billmills <you@example.com>
2023-07-26 16:58:54 -0400

    improvements, proofreading

--- glodap-sync.py
+++ glodap-sync.py
@@ -1,7 +1,7 @@
 # usage: python glodap-sync.py <first longitude index [0,360)> <last longitude index [0,360)> 
 import xarray as xr
 from pymongo import MongoClient
-import datetime, math, sys
+import datetime, math, sys, itertools, numpy
 from geopy import distance
 
 def tidylon(longitude):
@@ -65,7 +65,9 @@ if sys.argv[1] == 'meta':
 			[],
 			['units', 'long_name'],
 			[]
-		]
+		],
+		'snr': {},
+		'cl': {}
 	}
 
 	summary = {
@@ -75,6 +77,8 @@ if sys.argv[1] == 'meta':
 	}
 	for var in glodapvars:
 		ds = xr.open_dataset('/tmp/glodap/GLODAPv2.2016b.'+var+'.nc')
+		metadata['snr'][var] = ds['SnR'][0].to_dict()['data']
+		metadata['cl'][var] = ds['CL'][0].to_dict()['data']
 		# each file has a measurement, error, input mean, input std, input n and relative error
 		elements = [var, var+'_error', 'Input_mean', 'Input_std', 'Input_N', var+'_relerr']
 		renamed_elements = [var, var+'_error', var+'_Input_mean', var+'_Input_std', var+'_Input_N', var+'_relerr']
@@ -82,57 +86,55 @@ if sys.argv[1] == 'meta':
 		for e in elements:
 			metadata['data_info'][2].append( [ ds[e].attrs['units'], ds[e].attrs['long_name'] ] )
 	summary['data'] = metadata['data_info'][0]
-	for lonidx in range(360):
-		for latidx in range(180):
-			lon = tidylon(float(ds['lon'][lonidx].data) % 360) # mod since glodap is on [20,380]
-			lat = float(ds['lat'][latidx].data)
-			summary['lattice'].append([lon,lat])		
-
+	summary['lattice'] = [x['geolocation']['coordinates'] for x in list(db['glodap'].find({},{'geolocation':1}))]
+		
 	try:
 		db['glodapMeta'].replace_one({'_id': 'glodapv2.2016b'},metadata, True)
 		db['summaries'].replace_one({'_id': 'glodapsummary'},summary, True)
 	except BaseException as err:
 		print('error: db write failure')
 		print(err)
-
-# data construction
-data = []
-for lonidx in range(360):
-	data.append([])
-	for latidx in range(180):
-		lon = tidylon(float(ds['lon'][lonidx].data) % 360) # mod since glodap is on [20,380]
-		lat = float(ds['lat'][latidx].data)
-		data[lonidx].append({
-			'_id': str(lon)+'_'+str(lat),
-			'metadata': 'glodapv2.2016b',
-			'geolocation': {"type":"Point", "coordinates":[lon,lat]},
-			'basin': find_basin(basins, lon, lat),
-			'timestamp': 99,
-			'data': []
-		})
-
-# made the whole map above, but only populate some of it to sneak under memory constraints
-start_lonidx = int(sys.argv[1])
-end_lonidx = int(sys.argv[2])
-
-for var in glodapvars:
-	print('var extraction', var)
-	ds = xr.open_dataset('/tmp/glodap/GLODAPv2.2016b.'+var+'.nc')
-	elements = [var, var+'_error', 'Input_mean', 'Input_std', 'Input_N', var+'_relerr']
-	for lonidx in range(start_lonidx, end_lonidx):
-		print('longitude idx', lonidx)
+else:
+	# data construction
+	data = []
+	ds = xr.open_dataset('/tmp/glodap/GLODAPv2.2016b.Cant.nc')
+	for lonidx in range(360):
+		data.append([])
 		for latidx in range(180):
-			for e in elements:
-				data[lonidx][latidx]['data'].append(ds[e][:,latidx,lonidx].to_dict()['data'])
+			lon = tidylon(float(ds['lon'][lonidx].data) % 360) # mod since glodap is on [20,380]
+			lat = float(ds['lat'][latidx].data)
+			data[lonidx].append({
+				'_id': str(lon)+'_'+str(lat),
+				'metadata': 'glodapv2.2016b',
+				'geolocation': {"type":"Point", "coordinates":[lon,lat]},
+				'basin': find_basin(basins, lon, lat),
+				'timestamp': datetime.datetime(year=1, month=1, day=1),
+				'data': []
+			})
 
-for lonidx in range(start_lonidx, end_lonidx):
-	for latidx in range(180):
-		try:
-			db['glodap'].insert_one(data[lonidx][latidx])
-		except BaseException as err:
-			print('error: db write failure')
-			print(err)
-			print(data[lonidx][latidx])
+	# made the whole map above, but only populate some of it to sneak under memory constraints
+	start_lonidx = int(sys.argv[1])
+	end_lonidx = int(sys.argv[2])
+
+	for var in glodapvars:
+		print('var extraction', var)
+		ds = xr.open_dataset('/tmp/glodap/GLODAPv2.2016b.'+var+'.nc')
+		elements = [var, var+'_error', 'Input_mean', 'Input_std', 'Input_N', var+'_relerr']
+		for lonidx in range(start_lonidx, end_lonidx):
+			print('longitude idx', lonidx)
+			for latidx in range(180):
+				for e in elements:
+					data[lonidx][latidx]['data'].append(ds[e][:,latidx,lonidx].to_dict()['data'])
+
+	for lonidx in range(start_lonidx, end_lonidx):
+		for latidx in range(180):
+			if not numpy.isnan(list(itertools.chain.from_iterable(data[lonidx][latidx]['data']))).all():
+				try:
+					db['glodap'].insert_one(data[lonidx][latidx])
+				except BaseException as err:
+					print('error: db write failure')
+					print(err)
+					print(data[lonidx][latidx])
 
 
 
---
+++ proofread.py
@@ -0,0 +1,44 @@
+import xarray as xr
+from pymongo import MongoClient
+import random, time, numpy
+
+client = MongoClient('mongodb://database/argo')
+db = client.argo
+
+def tidylon(longitude):
+    # map longitude on [0,360] to [-180,180], required for mongo indexing
+    if longitude <= 180.0:
+        return longitude;
+    else:
+        return longitude-360.0;
+
+metadata = list(db['glodapMeta'].find({'_id': 'glodapv2.2016b'}))[0]
+
+while True:
+	latidx = random.randint(0,179)
+	lonidx = random.randint(0,359)
+	var = random.choice(['Cant', 'NO3', 'OmegaA', 'OmegaC', 'oxygen', 'pHts25p0', 'pHtsinsitutp', 'PI_TCO2', 'PO4', 'salinity', 'silicate', 'TAlk', 'TCO2', 'temperature'])
+	ds = xr.open_dataset('/tmp/glodap/GLODAPv2.2016b.'+var+'.nc')
+	element = random.choice([var, var+'_error', 'Input_mean', 'Input_std', 'Input_N', var+'_relerr'])
+	renamed_element = element
+	if not renamed_element.startswith(var):
+		renamed_element = var + '_' + renamed_element
+	print('checking', latidx, lonidx, element)
+
+	# original data from netcdf
+	column = ds[element][:,latidx,lonidx].to_dict()['data']
+
+	# look up corresponding data from mongodb
+	lon = tidylon(float(ds['lon'][lonidx].data) % 360) # mod since glodap is on [20,380]
+	lat = float(ds['lat'][latidx].data)
+	eltidx = metadata['data_info'][0].index(renamed_element)
+
+	document = list(db['glodap'].find({'_id': str(lon)+'_'+str(lat)}))
+	if(len(document) == 1):
+		document = document[0]
+		data = document['data'][eltidx]
+
+		if not numpy.allclose(data, column, atol=0.00000001, equal_nan=True):
+			print('mismatch', data, column)
+
+		time.sleep(5)
